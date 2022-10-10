9420a568cdc20b8d3de7b5008b615a9ea53c51ef
namhoai167 <namhoai167@gmail.com>
2022-10-10 18:34:09 +0700

    Add products display and dynamic links to product detail, and add homepage (#1)

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from products.views import home_view, products_display_view, product_detail_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('products/', products_display_view, name='products'),
+    path('products/<int:id>', product_detail_view, name='product-detail'),
     path('admin/', admin.site.urls),
 ]
