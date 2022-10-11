bc180e98cf31ea3083cdaa128cdf8eac486a1341
namvhnguyen <namhoai167@gmail.com>
2022-10-11 09:47:46 +0700

    Switch from SQLite to PostgreSQL

--- src/products/models.py
+++ src/products/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class Product(models.Model):
     title       = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    price       = models.DecimalField(decimal_places=2, max_digits=1000)
 
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'id': self.id})   # /products/{self.id}/
--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trydjango',
+        'USER': 'trydjango_admin',
+        'PASSWORD': 'trydjangopasswd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
