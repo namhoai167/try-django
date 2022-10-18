4e93464eef039be3ebeee1b4e3a904d88b72c280
namvhnguyen <namhoai167@gmail.com>
2022-10-18 14:26:41 +0700

    Remove views with Django template

--- src/products/views.py
+++ src/products/views.py
@@ -1,20 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
 
 # Create your views here.
 def home_view(*args, **kwargs):
     return HttpResponse('<h1>Welcome to homepage</h1>')
-
-def products_display_view(request):
-    context = {
-        'products': Product.objects.all()
-    }
-    return render(request, 'products/product_display.html', context)
-
-def product_detail_view(request, id):
-    context = {
-        'product': Product.objects.get(id=id)
-    }
-    return render(request, 'products/product_detail.html', context)
-    
