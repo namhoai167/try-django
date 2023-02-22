ba20ae4d977914ab24237c017fa8ab69d2cc03c8
unknown <namhoai167@google.com>
2023-02-22 16:22:49 +0700

    Use drf_spectacular for OpenAPI3 generation

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,11 @@
 # TODO Migrate to Router and Queryset
 
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 from .views import (
     ProductList,
@@ -10,4 +15,8 @@ from .views import (
 urlpatterns = [
     path('products/', ProductList.as_view(), name='api-products-list'),
     path('products/<int:id>', ProductDetail.as_view(), name='api-product-detail'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
